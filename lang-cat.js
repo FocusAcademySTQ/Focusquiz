2b0648efa945c500a845150bc51ce8a430f1868a
FocusAcademySTQ <focusacademyamc@gmail.com>
2025-09-28 10:33:19 +0200

    Update lang-cat.js

--- lang-cat.js
+++ lang-cat.js
@@ -134,15 +134,486 @@
 
   const BANK_MORFO_SUBJECTE = [
   { 
-    text: `Quin és el subjecte de: "Els alumnes estudien molt."`, 
-    options: ["Els alumnes", "molt", "estudien", "cap"], 
-    answer: `Els alumnes` 
-  },
-  { 
-    text: `Quin és el predicat de: "La Marta juga al parc."`, 
-    options: ["La Marta", "juga al parc", "al parc", "juga"], 
-    answer: `juga al parc` 
-  }
+  text: `Quin és el subjecte de: "La Maria canta una cançó."`, 
+  options: ["La Maria", "canta", "una cançó", "cançó"], 
+  answer: `La Maria` 
+},
+{ 
+  text: `Quin és el predicat de: "El gos corre pel jardí."`, 
+  options: ["El gos", "corre pel jardí", "pel jardí", "corre"], 
+  answer: `corre pel jardí` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els nens juguen a pilota."`, 
+  options: ["Els nens", "juguen", "a pilota", "pilota"], 
+  answer: `Els nens` 
+},
+{ 
+  text: `Quin és el predicat de: "L’avi llegeix el diari."`, 
+  options: ["L’avi", "llegeix el diari", "el diari", "llegeix"], 
+  answer: `llegeix el diari` 
+},
+{ 
+  text: `Quin és el subjecte de: "Les flors oloren bé."`, 
+  options: ["Les flors", "oloren", "bé", "cap"], 
+  answer: `Les flors` 
+},
+{ 
+  text: `Quin és el predicat de: "La nena dibuixa una casa."`, 
+  options: ["La nena", "dibuixa una casa", "una casa", "dibuixa"], 
+  answer: `dibuixa una casa` 
+},
+{ 
+  text: `Quin és el subjecte de: "El professor explica la lliçó."`, 
+  options: ["El professor", "explica", "la lliçó", "cap"], 
+  answer: `El professor` 
+},
+{ 
+  text: `Quin és el predicat de: "Els ocells volen pel cel."`, 
+  options: ["Els ocells", "volen pel cel", "volen", "pel cel"], 
+  answer: `volen pel cel` 
+},
+{ 
+  text: `Quin és el subjecte de: "El cotxe és vermell."`, 
+  options: ["El cotxe", "vermell", "és", "vermell i cotxe"], 
+  answer: `El cotxe` 
+},
+{ 
+  text: `Quin és el predicat de: "La Júlia menja pa amb oli."`, 
+  options: ["La Júlia", "menja pa amb oli", "menja", "pa amb oli"], 
+  answer: `menja pa amb oli` 
+},
+{ 
+  text: `Quin és el subjecte de: "El gat dorm al sofà."`, 
+  options: ["El gat", "dorm", "al sofà", "cap"], 
+  answer: `El gat` 
+},
+{ 
+  text: `Quin és el predicat de: "La mare prepara el dinar."`, 
+  options: ["La mare", "prepara el dinar", "el dinar", "prepara"], 
+  answer: `prepara el dinar` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els estudiants fan els deures."`, 
+  options: ["Els estudiants", "fan", "els deures", "cap"], 
+  answer: `Els estudiants` 
+},
+{ 
+  text: `Quin és el predicat de: "La pluja cau suaument."`, 
+  options: ["La pluja", "cau suaument", "cau", "suaument"], 
+  answer: `cau suaument` 
+},
+{ 
+  text: `Quin és el subjecte de: "La lluna il·lumina el bosc."`, 
+  options: ["La lluna", "il·lumina", "el bosc", "cap"], 
+  answer: `La lluna` 
+},
+{ 
+  text: `Quin és el predicat de: "Els professors corregeixen exàmens."`, 
+  options: ["Els professors", "corregeixen exàmens", "exàmens", "corregeixen"], 
+  answer: `corregeixen exàmens` 
+},
+{ 
+  text: `Quin és el subjecte de: "La Marta escriu una carta."`, 
+  options: ["La Marta", "escriu", "una carta", "carta"], 
+  answer: `La Marta` 
+},
+{ 
+  text: `Quin és el predicat de: "Els jugadors entrenen cada tarda."`, 
+  options: ["Els jugadors", "entrenen cada tarda", "entrenen", "cada tarda"], 
+  answer: `entrenen cada tarda` 
+},
+{ 
+  text: `Quin és el subjecte de: "El tren arriba puntual."`, 
+  options: ["El tren", "arriba", "puntual", "cap"], 
+  answer: `El tren` 
+},
+{ 
+  text: `Quin és el predicat de: "La nena somriu feliç."`, 
+  options: ["La nena", "somriu feliç", "somriu", "feliç"], 
+  answer: `somriu feliç` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els peixos neden ràpid."`, 
+  options: ["Els peixos", "naden", "ràpid", "cap"], 
+  answer: `Els peixos` 
+},
+{ 
+  text: `Quin és el predicat de: "El sol escalfa la platja."`, 
+  options: ["El sol", "escalfa la platja", "la platja", "escalfa"], 
+  answer: `escalfa la platja` 
+},
+{ 
+  text: `Quin és el subjecte de: "La bicicleta té tres rodes."`, 
+  options: ["La bicicleta", "té", "tres rodes", "cap"], 
+  answer: `La bicicleta` 
+},
+{ 
+  text: `Quin és el predicat de: "Els nens riuen contents."`, 
+  options: ["Els nens", "riuen contents", "riuen", "contents"], 
+  answer: `riuen contents` 
+},
+{ 
+  text: `Quin és el subjecte de: "El rellotge marca les dotze."`, 
+  options: ["El rellotge", "marca", "les dotze", "cap"], 
+  answer: `El rellotge` 
+},
+{ 
+  text: `Quin és el predicat de: "Les estrelles brillen al cel."`, 
+  options: ["Les estrelles", "brillen al cel", "brillen", "al cel"], 
+  answer: `brillen al cel` 
+},
+{ 
+  text: `Quin és el subjecte de: "La televisió funciona bé."`, 
+  options: ["La televisió", "funciona", "bé", "cap"], 
+  answer: `La televisió` 
+},
+{ 
+  text: `Quin és el predicat de: "El nen canta una cançó."`, 
+  options: ["El nen", "canta una cançó", "canta", "una cançó"], 
+  answer: `canta una cançó` 
+},
+{ 
+  text: `Quin és el subjecte de: "Les fulles cauen dels arbres."`, 
+  options: ["Les fulles", "cauen", "arbres", "cap"], 
+  answer: `Les fulles` 
+},
+{ 
+  text: `Quin és el predicat de: "Els gats cacen ratolins."`, 
+  options: ["Els gats", "cacen ratolins", "ratolins", "cacen"], 
+  answer: `cacen ratolins` 
+},
+{ 
+  text: `Quin és el subjecte de: "El mestre corregeix les redaccions."`, 
+  options: ["El mestre", "corregeix", "les redaccions", "cap"], 
+  answer: `El mestre` 
+},
+{ 
+  text: `Quin és el predicat de: "La Maria compra fruita fresca."`, 
+  options: ["La Maria", "compra fruita fresca", "compra", "fruita fresca"], 
+  answer: `compra fruita fresca` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els avis caminen pel parc."`, 
+  options: ["Els avis", "caminen", "pel parc", "cap"], 
+  answer: `Els avis` 
+},
+{ 
+  text: `Quin és el predicat de: "La Júlia pinta un quadre."`, 
+  options: ["La Júlia", "pinta un quadre", "pinta", "un quadre"], 
+  answer: `pinta un quadre` 
+},
+{ 
+  text: `Quin és el subjecte de: "El telèfon sona fort."`, 
+  options: ["El telèfon", "sona", "fort", "cap"], 
+  answer: `El telèfon` 
+},
+{ 
+  text: `Quin és el predicat de: "Els alumnes escolten la lliçó."`, 
+  options: ["Els alumnes", "escolten la lliçó", "escolten", "lliçó"], 
+  answer: `escolten la lliçó` 
+},
+{ 
+  text: `Quin és el subjecte de: "El riu porta molta aigua."`, 
+  options: ["El riu", "porta", "aigua", "cap"], 
+  answer: `El riu` 
+},
+{ 
+  text: `Quin és el predicat de: "Els cotxes circulen ràpid."`, 
+  options: ["Els cotxes", "circulen ràpid", "circulen", "ràpid"], 
+  answer: `circulen ràpid` 
+},
+{ 
+  text: `Quin és el subjecte de: "La biblioteca té molts llibres."`, 
+  options: ["La biblioteca", "té", "llibres", "cap"], 
+  answer: `La biblioteca` 
+},
+{ 
+  text: `Quin és el predicat de: "Els estudiants llegeixen en silenci."`, 
+  options: ["Els estudiants", "llegeixen en silenci", "llegeixen", "en silenci"], 
+  answer: `llegeixen en silenci` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els gossos borden fort."`, 
+  options: ["Els gossos", "borden", "fort", "cap"], 
+  answer: `Els gossos` 
+},
+{ 
+  text: `Quin és el predicat de: "La mare renta els plats."`, 
+  options: ["La mare", "renta els plats", "renta", "els plats"], 
+  answer: `renta els plats` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els llibres estan a la taula."`, 
+  options: ["Els llibres", "estan", "a la taula", "cap"], 
+  answer: `Els llibres` 
+},
+{ 
+  text: `Quin és el predicat de: "El vent bufa fort."`, 
+  options: ["El vent", "bufa fort", "bufa", "fort"], 
+  answer: `bufa fort` 
+},
+{ 
+  text: `Quin és el subjecte de: "Les nenes ballen a l’escenari."`, 
+  options: ["Les nenes", "ballen", "a l’escenari", "cap"], 
+  answer: `Les nenes` 
+},
+{ 
+  text: `Quin és el predicat de: "El noi obre la porta."`, 
+  options: ["El noi", "obre la porta", "obre", "porta"], 
+  answer: `obre la porta` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els ocells canten alegres."`, 
+  options: ["Els ocells", "canten", "alegres", "cap"], 
+  answer: `Els ocells` 
+},
+{ 
+  text: `Quin és el predicat de: "La mestra escriu a la pissarra."`, 
+  options: ["La mestra", "escriu a la pissarra", "escriu", "pissarra"], 
+  answer: `escriu a la pissarra` 
+}
+{ 
+  text: `Quin és el subjecte de: "El pare condueix el cotxe."`, 
+  options: ["El pare", "condueix", "el cotxe", "cap"], 
+  answer: `El pare` 
+},
+{ 
+  text: `Quin és el predicat de: "La Clara prepara un pastís."`, 
+  options: ["La Clara", "prepara un pastís", "prepara", "un pastís"], 
+  answer: `prepara un pastís` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els núvols tapen el sol."`, 
+  options: ["Els núvols", "tapen", "el sol", "cap"], 
+  answer: `Els núvols` 
+},
+{ 
+  text: `Quin és el predicat de: "El gos busca la pilota."`, 
+  options: ["El gos", "busca la pilota", "busca", "la pilota"], 
+  answer: `busca la pilota` 
+},
+{ 
+  text: `Quin és el subjecte de: "La Maria llegeix un llibre."`, 
+  options: ["La Maria", "llegeix", "un llibre", "cap"], 
+  answer: `La Maria` 
+},
+{ 
+  text: `Quin és el predicat de: "Els nens pinten dibuixos."`, 
+  options: ["Els nens", "pinten dibuixos", "pinten", "dibuixos"], 
+  answer: `pinten dibuixos` 
+},
+{ 
+  text: `Quin és el subjecte de: "El gosset juga al jardí."`, 
+  options: ["El gosset", "juga", "al jardí", "cap"], 
+  answer: `El gosset` 
+},
+{ 
+  text: `Quin és el predicat de: "La Júlia obre la finestra."`, 
+  options: ["La Júlia", "obre la finestra", "obre", "finestra"], 
+  answer: `obre la finestra` 
+},
+{ 
+  text: `Quin és el subjecte de: "Les flors decoren la taula."`, 
+  options: ["Les flors", "decoren", "la taula", "cap"], 
+  answer: `Les flors` 
+},
+{ 
+  text: `Quin és el predicat de: "El nen trenca el paper."`, 
+  options: ["El nen", "trenca el paper", "trenca", "paper"], 
+  answer: `trenca el paper` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els avis expliquen històries."`, 
+  options: ["Els avis", "expliquen", "històries", "cap"], 
+  answer: `Els avis` 
+},
+{ 
+  text: `Quin és el predicat de: "La mare cus un vestit."`, 
+  options: ["La mare", "cus un vestit", "cus", "vestit"], 
+  answer: `cus un vestit` 
+},
+{ 
+  text: `Quin és el subjecte de: "El cotxe aparca davant l’escola."`, 
+  options: ["El cotxe", "aparca", "davant l’escola", "cap"], 
+  answer: `El cotxe` 
+},
+{ 
+  text: `Quin és el predicat de: "Els nens obren els regals."`, 
+  options: ["Els nens", "obren els regals", "obren", "regals"], 
+  answer: `obren els regals` 
+},
+{ 
+  text: `Quin és el subjecte de: "La llum entra per la finestra."`, 
+  options: ["La llum", "entra", "finestra", "cap"], 
+  answer: `La llum` 
+},
+{ 
+  text: `Quin és el predicat de: "Els gossos mengen carn."`, 
+  options: ["Els gossos", "mengen carn", "mengen", "carn"], 
+  answer: `mengen carn` 
+},
+{ 
+  text: `Quin és el subjecte de: "El nen juga a l’ordinador."`, 
+  options: ["El nen", "juga", "a l’ordinador", "cap"], 
+  answer: `El nen` 
+},
+{ 
+  text: `Quin és el predicat de: "La mestra ajuda els alumnes."`, 
+  options: ["La mestra", "ajuda els alumnes", "ajuda", "alumnes"], 
+  answer: `ajuda els alumnes` 
+},
+{ 
+  text: `Quin és el subjecte de: "El pintor pinta un quadre."`, 
+  options: ["El pintor", "pinta", "un quadre", "cap"], 
+  answer: `El pintor` 
+},
+{ 
+  text: `Quin és el predicat de: "Els músics toquen la cançó."`, 
+  options: ["Els músics", "toquen la cançó", "toquen", "cançó"], 
+  answer: `toquen la cançó` 
+},
+{ 
+  text: `Quin és el subjecte de: "El foc crema la fusta."`, 
+  options: ["El foc", "crema", "la fusta", "cap"], 
+  answer: `El foc` 
+},
+{ 
+  text: `Quin és el predicat de: "Els nens miren la televisió."`, 
+  options: ["Els nens", "miren la televisió", "miren", "televisió"], 
+  answer: `miren la televisió` 
+},
+{ 
+  text: `Quin és el subjecte de: "El mar cobreix la sorra."`, 
+  options: ["El mar", "cobreix", "sorra", "cap"], 
+  answer: `El mar` 
+},
+{ 
+  text: `Quin és el predicat de: "La nena beu aigua."`, 
+  options: ["La nena", "beu aigua", "beu", "aigua"], 
+  answer: `beu aigua` 
+},
+{ 
+  text: `Quin és el subjecte de: "Les ovelles mengen herba."`, 
+  options: ["Les ovelles", "mengen", "herba", "cap"], 
+  answer: `Les ovelles` 
+},
+{ 
+  text: `Quin és el predicat de: "Els avis seuen al banc."`, 
+  options: ["Els avis", "seuen al banc", "seuen", "banc"], 
+  answer: `seuen al banc` 
+},
+{ 
+  text: `Quin és el subjecte de: "El camió porta fusta."`, 
+  options: ["El camió", "porta", "fusta", "cap"], 
+  answer: `El camió` 
+},
+{ 
+  text: `Quin és el predicat de: "La Júlia escriu un poema."`, 
+  options: ["La Júlia", "escriu un poema", "escriu", "poema"], 
+  answer: `escriu un poema` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els nens aprenen matemàtiques."`, 
+  options: ["Els nens", "aprenen", "matemàtiques", "cap"], 
+  answer: `Els nens` 
+},
+{ 
+  text: `Quin és el predicat de: "El noi juga a futbol."`, 
+  options: ["El noi", "juga a futbol", "juga", "futbol"], 
+  answer: `juga a futbol` 
+},
+{ 
+  text: `Quin és el subjecte de: "La mare prepara cafè."`, 
+  options: ["La mare", "prepara", "cafè", "cap"], 
+  answer: `La mare` 
+},
+{ 
+  text: `Quin és el predicat de: "Els nens corren al pati."`, 
+  options: ["Els nens", "corren al pati", "corren", "pati"], 
+  answer: `corren al pati` 
+},
+{ 
+  text: `Quin és el subjecte de: "La nena toca el piano."`, 
+  options: ["La nena", "toca", "piano", "cap"], 
+  answer: `La nena` 
+},
+{ 
+  text: `Quin és el predicat de: "El gos mossega l’os."`, 
+  options: ["El gos", "mossega l’os", "mossega", "os"], 
+  answer: `mossega l’os` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els avions volen alt."`, 
+  options: ["Els avions", "volen", "alt", "cap"], 
+  answer: `Els avions` 
+},
+{ 
+  text: `Quin és el predicat de: "El nen menja galetes."`, 
+  options: ["El nen", "menja galetes", "menja", "galetes"], 
+  answer: `menja galetes` 
+},
+{ 
+  text: `Quin és el subjecte de: "La professora escriu la data."`, 
+  options: ["La professora", "escriu", "data", "cap"], 
+  answer: `La professora` 
+},
+{ 
+  text: `Quin és el predicat de: "Els arbres donen ombra."`, 
+  options: ["Els arbres", "donen ombra", "donen", "ombra"], 
+  answer: `donen ombra` 
+},
+{ 
+  text: `Quin és el subjecte de: "El nen llegeix un conte."`, 
+  options: ["El nen", "llegeix", "conte", "cap"], 
+  answer: `El nen` 
+},
+{ 
+  text: `Quin és el predicat de: "La mare cuina arròs."`, 
+  options: ["La mare", "cuina arròs", "cuina", "arròs"], 
+  answer: `cuina arròs` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els cotxes fan soroll."`, 
+  options: ["Els cotxes", "fan", "soroll", "cap"], 
+  answer: `Els cotxes` 
+},
+{ 
+  text: `Quin és el predicat de: "El gat caça un ratolí."`, 
+  options: ["El gat", "caça un ratolí", "caça", "ratolí"], 
+  answer: `caça un ratolí` 
+},
+{ 
+  text: `Quin és el subjecte de: "Les estrelles il·luminen la nit."`, 
+  options: ["Les estrelles", "il·luminen", "nit", "cap"], 
+  answer: `Les estrelles` 
+},
+{ 
+  text: `Quin és el predicat de: "El nen porta la motxilla."`, 
+  options: ["El nen", "porta la motxilla", "porta", "motxilla"], 
+  answer: `porta la motxilla` 
+},
+{ 
+  text: `Quin és el subjecte de: "El professor explica història."`, 
+  options: ["El professor", "explica", "història", "cap"], 
+  answer: `El professor` 
+},
+{ 
+  text: `Quin és el predicat de: "La nena pinta flors."`, 
+  options: ["La nena", "pinta flors", "pinta", "flors"], 
+  answer: `pinta flors` 
+},
+{ 
+  text: `Quin és el subjecte de: "Els soldats marxen units."`, 
+  options: ["Els soldats", "marxen", "units", "cap"], 
+  answer: `Els soldats` 
+},
+{ 
+  text: `Quin és el predicat de: "El gos dorm tranquil."`, 
+  options: ["El gos", "dorm tranquil", "dorm", "tranquil"], 
+  answer: `dorm tranquil` 
+}
+
 ];
 
 const BANK_MORFO_TEMPS = [
