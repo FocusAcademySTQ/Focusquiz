b19a57fe9e2e1e14430a1fd81bb1267f71ed9349
FocusAcademySTQ <focusacademyamc@gmail.com>
2025-10-29 10:24:31 +0100

    Update supabase-config.js

--- portal/supabase-config.js
+++ portal/supabase-config.js
@@ -1,3 +1,2 @@
 export const SUPABASE_URL = window?.SUPABASE_URL || '';
 export const SUPABASE_ANON_KEY = window?.SUPABASE_ANON_KEY || '';
-export const SUPABASE_TEACHER_CODE = window?.SUPABASE_TEACHER_CODE || '';
